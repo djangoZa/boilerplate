dd597dc4b67a47f71c0ca5c73b1c0622a0e557fb
Mike <mikejohnclarke@gmail.com>
2014-04-09 15:27:34 -0300

    initializing boilerplate

--- project/application/controllers/ErrorController.php
+++ project/application/controllers/ErrorController.php
@@ -4,7 +4,7 @@ class ErrorController extends Zend_Controller_Action
     public function errorAction()
     {
         $errors = $this->_getParam('error_handler');
- 
+
         switch ($errors->type) {
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
@@ -19,7 +19,7 @@ class ErrorController extends Zend_Controller_Action
                 $this->view->message = 'Application error';
                 break;
         }
- 
+
         $this->view->exception = $errors->exception;
         $this->view->request   = $errors->request;
     }
--- project/public/index.php
+++ project/public/index.php
@@ -1,13 +1,11 @@
 <?php
 //Define path to application directory
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH',
-        realpath(dirname(__FILE__) . '/../application'));
- 
+defined('APPLICATION_PATH') || 
+    define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
 //Define application environment
-defined('APPLICATION_ENV')
-	|| define('APPLICATION_ENV',
-        (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+defined('APPLICATION_ENV') || 
+    define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
 //Set the include paths
 set_include_path(implode(PATH_SEPARATOR, $paths = array(
